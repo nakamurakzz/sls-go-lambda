8fa48da38ad4bd03f5a0e2db55668f28d87550f4
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-04-11 06:32:30 +0900

    条件変更

--- script/upload.sh
+++ script/upload.sh
@@ -5,7 +5,7 @@ BUCKET="dev-sls-s3-event-lambda"
 echo "Input AWS_PROFILE"
 read AWS_PROFILE
 
-for i in {1..50}; do
+for i in {1..10}; do
   echo "Uploading file for loop $i"
   echo "Loop: $i" > test.txt
   aws s3 cp test.txt s3://${BUCKET}/hello/test_loop_${i}.txt --profile ${AWS_PROFILE}
