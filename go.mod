863226b67abaa42c4ba3813ed7204029951e0a92
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-04-10 19:59:30 +0900

    S3 EventでLambdaを起動する

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/nakamurakzz/sls-go-lambda
 go 1.19
 
 require github.com/aws/aws-lambda-go v1.39.1
+
+require (
+	github.com/aws/aws-sdk-go v1.44.239 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,15 +1,66 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"log"
+	"time"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func HandleMain() error {
-	fmt.Println("Hello World!")
+// Event: S3
+func handleS3Event(content context.Context, s3Event events.S3Event ) error {
+	log.Println(content, s3Event)
+	for _, record := range s3Event.Records {
+		s3Entity := record.S3
+		bucket := s3Entity.Bucket.Name
+		key := s3Entity.Object.Key
+
+		fileContent, err := readFileFromS3(bucket, key)
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("File content: %s\n", fileContent)
+
+		log.Println("Sleeping for 5 seconds...")
+		time.Sleep(10 * time.Second)
+		log.Println("Done")
+	}
+
 	return nil
 }
+
+func readFileFromS3(bucket, key string) (string, error) {
+	s := session.Must(session.NewSession())
+	s3Client := s3.New(s)
+
+	input := &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	}
+
+	result, err := s3Client.GetObject(input)
+	if err != nil {
+		return "", err
+	}
+
+	defer result.Body.Close()
+
+	content, err := io.ReadAll(result.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+}
+
 func main() {
-	lambda.Start(HandleMain)
+	lambda.Start(handleS3Event)
 }
